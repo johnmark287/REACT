fc5db130a6e0794bf8d9533d3d6b87465af59310
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2024-02-29 17:44:02 +0300

    feat: create api app

--- django-react/backend/backend/settings.py
+++ django-react/backend/backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Rest Framework
+    'rest_framework',
+    
+    # Custom apps
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
